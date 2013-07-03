df284cae4ac1bf6652458c735fec82aaa43b7494
Mico <mico_dls@yahoo.com>
2013-07-03 15:51:10 +0800

    Default controller

---
+++ application/controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php 
+
+class PageController extends CI_Controller{
+    public function index(){
+        $this->load->view("index");
+    }
+}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
