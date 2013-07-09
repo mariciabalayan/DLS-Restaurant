9c9c1cb76665f2b986905b4d103d60b4a20c0a64
Mico <mico_dls@yahoo.com>
2013-07-09 13:06:58 +0800

    Fixed file linking

--- application/views/template.php
+++ application/views/template.php
@@ -1,6 +1,6 @@
 <?php
     $this->load->view('includes/header');
     $this->load->view('includes/navbar');
-    $this->load->view($main_content);
+    $this->load->view('pages/'.$main_content);
     $this->load->view('includes/footer');
 ?>
